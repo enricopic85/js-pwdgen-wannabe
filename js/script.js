874a3d6d1a86f51f7ae673605080272ef046c1f4
enricopic85 <enricodev85@gmail.com>
2021-12-02 14:37:20 +0100

    risoluzione esercizio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // chiedo il nome all'utente
-
+const userName=prompt("inserisci il tuo nome");
 // chiedo il cognome
-
+const surname=prompt("inserisci il tuo cognome");
 //chiedo il colore preferito
-
+const favoriteColor=prompt("inserisci il tuo colore preferito");
 //scrivo sulla pagina nomecognomecolorepreferito21
+let htmlElement=document.getElementById('password');
+htmlElement.innerHTML= `${userName}${surname}${favoriteColor}21`
+
+alert("Mr Robot scansate proprio");
