51939fa8f903cd50d4b66f70d453be9d2e8a6bec
enricopic85 <enricodev85@gmail.com>
2021-12-02 16:57:53 +0100

    bonus colore utente

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -14,7 +14,26 @@ let somma= (4*8) + (8/2);
 console.log(somma); 
 
 //concateniamo 2 stringhe
-let primaFrase="Sempre caro mi fu ";
+let primaFrase="Sempre caro mi fu";
 let secondaFrase="quest'ermo colle";
 let leopardi=`${primaFrase} ${secondaFrase}`;
 console.log(leopardi);
+
+//inseriamo i dati di un utente(l'ho commentato per impedire l'avvio ad ogni apertura)
+// let nome=prompt("inserisci il tuo nome")
+// document.getElementById("nome").innerHTML = nome;
+// let cognome=prompt("inserisci il tuo cognome")
+// document.getElementById("cognome").innerHTML = cognome;
+// let indirizzo=prompt("inserisci il tuo indirizzo")
+// document.getElementById("indirizzo").innerHTML = indirizzo;
+// let telefono=prompt("inserisci il tuo telefono")
+// document.getElementById("telefono").innerHTML = telefono;
+
+//cambiamo il colore del testo in rosso
+var line=document.getElementById("line");
+line.style.color="red";
+line.style.backgroundColor="grey";
+
+//chiedo il colore all'utente e lo uso per settare il colore del testo
+let coloreTesto= prompt("inserisci il colore");
+document.getElementById("colore").style.color=coloreTesto;
